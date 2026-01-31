ee41a74455ca1c744221efbc7ac7aecd9ff4b6d8
Ubuntu <ubuntu@ip-172-31-24-172.us-west-2.compute.internal>
2026-01-31 20:25:13 +0000

    Finished rest framework setup

--- myproject/restaurant/models.py
+++ myproject/restaurant/models.py
@@ -13,17 +13,12 @@ class Restaurant(models.Model):
     def __str__(self):
         return self.RestaurantName
 
-
 class Customer(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=15, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
-    
-class Reservation(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    reservation_date = models.DateField()
-    reservation_time = models.TimeField()
