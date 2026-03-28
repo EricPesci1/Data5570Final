6f8b587d0ab93f96be15195f802dbc31240b98d0
Eric Pesci <ericpesci04@gmail.com>
2026-03-28 15:12:12 -0600

    Updating CORS

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -42,11 +42,14 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8081',
+    'http://127.0.0.1:8081',
+]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
